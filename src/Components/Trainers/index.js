58d4cff154f047cbb50864f226012a2e582b468f
Galo Durante <durantegalo@gmail.com>
2023-05-28 20:48:26 -0300

    MR-53: create POST method

--- src/Components/Trainers/index.js
+++ src/Components/Trainers/index.js
@@ -35,24 +35,63 @@ function Trainers() {
     }
   };
 
-  // const createTrainer = async () => {
-  //   const form = document.getElementById('form');
-  //   const name = form.elements.name.value;
-  //   const email = form.elements.email.value;
+  const createTrainer = async () => {
+    console.log(formValue);
+    try {
+      const createdTrainerResponse = await fetch(`${process.env.REACT_APP_API_URL}/trainers/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formValue)
+      });
+      const createdTrainer = await createdTrainerResponse.json();
+      console.log(createdTrainer);
+      setTrainers((currentTrainers) => {
+        return [...currentTrainers, createdTrainer.data];
+      });
+      setFormValue({
+        firstName: '',
+        lastName: '',
+        dni: '',
+        phone: '',
+        email: '',
+        city: '',
+        password: '',
+        salary: ''
+      });
+    } catch (error) {
+      console.error(error);
+      // show error in UI
+    }
+  };
 
-  //   const data = {
-  //     firstName: name,
-  //     email: email
-  //   };
-
+  // const updateTrainer = async () => {
+  //   console.log(formValue);
   //   try {
-  //     const createdTrainer = await fetch(`${process.env.REACT_APP_API_URL}/trainers/`, {
-  //       method: 'POST',
-  //       body: JSON.stringify(data)
+  //     const updatedTrainerResponse = await fetch(`${process.env.REACT_APP_API_URL}/trainers/`, {
+  //       method: 'PUT',
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       },
+  //       body: JSON.stringify(formValue)
   //     });
 
+  //     const updatedTrainer = await updatedTrainerResponse.json();
+  //     console.log(updatedTrainer);
   //     setTrainers((currentTrainers) => {
-  //       return [...currentTrainers, createdTrainer]; // Not sure if this is right
+  //       return [...currentTrainers, updatedTrainer.data];
+  //     });
+  //     setFormValue({
+  //       firstName: '',
+  //       lastName: '',
+  //       dni: '',
+  //       phone: '',
+  //       email: '',
+  //       city: '',
+  //       password: '',
+  //       salary: '',
+  //       isActive: ''
   //     });
   //   } catch (error) {
   //     console.error(error);
@@ -60,88 +99,176 @@ function Trainers() {
   //   }
   // };
 
+  const [formValue, setFormValue] = useState({
+    firstName: '',
+    lastName: '',
+    dni: '',
+    phone: '',
+    email: '',
+    city: '',
+    password: '',
+    salary: ''
+  });
+
+  const [isVisible, setIsVisible] = useState(false);
+  const [buttonAddIsVisible, setAddVisible] = useState(false);
+  const [buttonSaveIsVisible, setSaveVisible] = useState(false);
+
+  const onChangeInput = (e) => {
+    setFormValue({
+      ...formValue,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    createTrainer();
+    formInvisible();
+  };
+
+  const save = () => {
+    //updateTrainer();
+    formInvisible();
+  };
+
+  const create = () => {
+    formVisible();
+    addVisible();
+  };
+
+  function modify() {
+    formVisible();
+    saveVisible();
+  }
+
+  const formVisible = () => {
+    setIsVisible(true);
+  };
+
+  const formInvisible = () => {
+    setIsVisible(false);
+  };
+
+  const addVisible = () => {
+    setAddVisible(true);
+    setSaveVisible(false);
+  };
+
+  const saveVisible = () => {
+    setAddVisible(false);
+    setSaveVisible(true);
+  };
+
   useEffect(() => {
     getTrainers();
   }, []);
-
-  // const [trainer, setTrainer] = useState({
-  //   name: '',
-  //   email: ''
-  // });
-
-  // const onChangeInput = (e) => {
-  //   setTrainer({
-  //     ...trainer,
-  //     [e.target.name]: e.target.value
-  //   });
-  // };
-
-  // const onSubmit = (e) => {
-  //   e.preventDefault();
-  //   createTrainer(trainer);
-  //   setTrainer({
-  //     name: '',
-  //     email: ''
-  //   });
-  // };
-
   return (
     <section className={styles.container}>
-      <h2>Trainers</h2>
-      <button>Create</button>
-      <table>
-        <thead>
-          <tr>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {trainers.map((trainer) => {
-            return (
-              <tr key={trainer._id}>
-                <td>{trainer.firstName}</td>
-                <td>{trainer.email}</td>
-                <td></td>
-                <td>
-                  <button
-                    className={styles.deleteButton}
-                    onClick={() => deleteTrainer(trainer._id)}
-                  >
-                    X
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <form className={styles.form} /*onSubmit={onSubmit}*/ id="form">
-        <div className={styles.subContainer}>
-          <div className={styles.inputContainer}>
-            <label className={styles.label}>Name</label>
-            <input
-              className={styles.input}
-              name="name"
-              type="text"
-              value={trainers.name}
-              //onChange={onChangeInput}
-            />
-          </div>
-          <div className={styles.inputContainer}>
-            <label className={styles.label}>Email</label>
-            <input
-              className={styles.input}
-              name="email"
-              type="text"
-              value={trainers.email}
-              //onChange={onChangeInput}
-            />
-          </div>
-        </div>
-        <button className={styles.button} type="submit">
-          Add
-        </button>
-      </form>
+      <section>
+        <h2>Trainers</h2>
+        <button onClick={create}>Create</button>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {trainers.map((trainer) => {
+              return (
+                <tr key={trainer._id}>
+                  <td>{trainer.firstName}</td>
+                  <td>{trainer.email}</td>
+                  <td>{trainer.phone}</td>
+                  <td>{trainer.city}</td>
+                  <td>
+                    <button className={styles.updateButton} onClick={modify}>
+                      Modify
+                    </button>
+                    <button
+                      className={styles.deleteButton}
+                      onClick={() => deleteTrainer(trainer._id)}
+                    >
+                      X
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </section>
+      {isVisible && (
+        <section className={styles.sectionForm}>
+          <form className={styles.form} onSubmit={onSubmit} id="form">
+            <div className={styles.subContainer}>
+              <div className={styles.inputContainer}>
+                <label className={styles.label}>Name</label>
+                <input
+                  className={styles.input}
+                  name="firstName"
+                  type="text"
+                  onChange={onChangeInput}
+                />
+              </div>
+              <div className={styles.inputContainer}>
+                <label className={styles.label}>Last Name</label>
+                <input
+                  className={styles.input}
+                  name="lastName"
+                  type="text"
+                  onChange={onChangeInput}
+                />
+              </div>
+              <div className={styles.inputContainer}>
+                <label className={styles.label}>DNI</label>
+                <input className={styles.input} name="dni" type="text" onChange={onChangeInput} />
+              </div>
+              <div className={styles.inputContainer}>
+                <label className={styles.label}>Phone</label>
+                <input className={styles.input} name="phone" type="text" onChange={onChangeInput} />
+              </div>
+              <div className={styles.inputContainer}>
+                <label className={styles.label}>Email</label>
+                <input className={styles.input} name="email" type="text" onChange={onChangeInput} />
+              </div>
+              <div className={styles.inputContainer}>
+                <label className={styles.label}>City</label>
+                <input className={styles.input} name="city" type="text" onChange={onChangeInput} />
+              </div>
+              <div className={styles.inputContainer}>
+                <label className={styles.label}>Password</label>
+                <input
+                  className={styles.input}
+                  name="password"
+                  type="password"
+                  onChange={onChangeInput}
+                />
+              </div>
+              <div className={styles.inputContainer}>
+                <label className={styles.label}>Salary</label>
+                <input
+                  className={styles.input}
+                  name="salary"
+                  type="text"
+                  onChange={onChangeInput}
+                />
+              </div>
+            </div>
+            {buttonAddIsVisible && (
+              <button className={styles.button} type="submit">
+                Add
+              </button>
+            )}
+            {buttonSaveIsVisible && (
+              <button className={styles.button} onClick={save}>
+                Save
+              </button>
+            )}
+          </form>
+        </section>
+      )}
     </section>
   );
 }
