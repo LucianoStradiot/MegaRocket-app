06044e2af305bd402f9d8c2348f9be59e7a2960c
OscarLucero <olucero921@gmail.com>
2023-06-05 13:39:24 -0300

    MR-68: Performed Text Input

--- src/Components/Shared/TextInput/index.js
+++ src/Components/Shared/TextInput/index.js
@@ -1,11 +1,40 @@
+import { useState, useEffect } from 'react';
 import styles from './textInput.module.css';
 
-function TextInput() {
-  return (
-    <section className={styles.container}>
-      <h2>TextInput</h2>
-    </section>
-  );
+function textInput({ inputType, data, labelName, editMode, useStateItem }) {
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    if (editMode) {
+      switch (inputType) {
+        case 'text':
+          setText(data);
+          break;
+        default:
+          break;
+      }
+    } else {
+      setText('');
+    }
+  }, [data]);
+
+  const renderInputType = (inputType) => {
+    if (inputType === 'text') {
+      return (
+        <label className={styles['form-label']}>
+          <h2>{labelName}</h2>
+          <input
+            className={styles['form-input']}
+            type="text"
+            value={text}
+            onChange={(event) => useStateItem(event.target.value)}
+          />
+        </label>
+      );
+    }
+  };
+
+  return renderInputType(inputType);
 }
 
-export default TextInput;
+export default textInput;
