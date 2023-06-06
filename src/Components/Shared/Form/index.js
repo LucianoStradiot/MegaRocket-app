dd85ae2326b2936cd5a01986e5de4338731a7d39
Galo Durante <durantegalo@gmail.com>
2023-06-06 11:29:24 -0300

    MR-78: add shared components form and table

---
+++ src/Components/Shared/Form/index.js
@@ -0,0 +1,11 @@
+import styles from './form.module.css';
+
+const Form = () => {
+  return (
+    <section className={styles.container}>
+      <h1>Hola Form</h1>
+    </section>
+  );
+};
+
+export default Form;
---
+++ src/Components/Shared/Table/index.js
@@ -0,0 +1,11 @@
+import styles from './table.module.css';
+
+const Table = () => {
+  return (
+    <section className={styles.container}>
+      <h1>Hola Table</h1>
+    </section>
+  );
+};
+
+export default Table;
