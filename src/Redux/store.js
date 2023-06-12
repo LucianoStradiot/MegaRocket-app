aa8913407318211a024e56b5469f66392d22270c
JuanManuel-Blois <juan.m.blois@gmail.com>
2023-06-12 11:08:21 -0300

    MR-123: modified store.js to fix vercel

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,22 +1,22 @@
 import { combineReducers, legacy_createStore as createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import { adminsReducer } from './Admins/reducer';
-import { trainersReducer } from './Trainers/reducer';
-import { superAdminsReducer } from './SuperAdmins/reducer';
-import { membersReducer } from './Members/reducer';
-import { subscriptionsReducer } from './Subscriptions/reducer';
-import { classesReducer } from './Classes/reducer';
-import { activitiesReducer } from './Activities/reducer';
+// import { adminsReducer } from './Admins/reducer';
+// import { trainersReducer } from './Trainers/reducer';
+// import { superAdminsReducer } from './SuperAdmins/reducer';
+// import { membersReducer } from './Members/reducer';
+// import { subscriptionsReducer } from './Subscriptions/reducer';
+// import { classesReducer } from './Classes/reducer';
+// import { activitiesReducer } from './Activities/reducer';
 
 const rootReducer = combineReducers({
-  admins: adminsReducer,
-  superAdmins: superAdminsReducer,
-  trainers: trainersReducer,
-  members: membersReducer,
-  subscriptions: subscriptionsReducer,
-  classes: classesReducer,
-  activities: activitiesReducer
+  // admins: adminsReducer,
+  // superAdmins: superAdminsReducer,
+  // trainers: trainersReducer,
+  // members: membersReducer,
+  // subscriptions: subscriptionsReducer,
+  // classes: classesReducer,
+  // activities: activitiesReducer
 });
 
 const configureStore = () => {
