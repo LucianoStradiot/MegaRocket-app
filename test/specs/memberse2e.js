94a36744101215d4e173e6a259c8d21cd6aea399
Katerine Airala <katherine.airala@gmail.com>
2023-06-27 23:31:20 -0300

    BaSP-W15: added tests to schedule a class

--- test/specs/memberse2e.js
+++ test/specs/memberse2e.js
@@ -117,3 +117,16 @@ describe('Go to home page', () => {
   });
 });
 
+describe('Schedule a class', () => {
+  it('should go to the schedule page', async () => {
+     await HomePage.openHomePage();
+     await HomePage.scheduleButton.click();
+     await expect(browser).toHaveUrl('https://joaco-megarocket-app.vercel.app/schedule');
+   });
+   it('should select the spinning class', async () => {
+     await SchedulePage.spinningClass.click();
+     await expect(SchedulePage.modal).toBeDisplayed();
+     await SchedulePage.acceptBtn.click();
+    });
+ });
+ 
