72db3a303f4cd177546b606162c3db3948fdcb12
FlorFarruggia <[flor.farru@gmail.com]>
2023-07-01 12:46:46 -0300

    [QA]BaSP-WB1-test-Florencia: Save changes in order to bring the change of master to continuo making the test for Admin

--- test/pageobjects/classAdminPage.js
+++ test/pageobjects/classAdminPage.js
@@ -1,60 +1,72 @@
 class ClassesAdmin {
-  get buttonCreateClasses () {
-    return $('[data-testid="create-button-classes"]')
+  get buttonCreateClasses() {
+    return $('section a[href="/admins/classes/form"]');
  }
  get formClasses () {
-     return $('[data-testid="form-classes"]')
+     return $('[data-testid="form-classes"]');
   }
   get selectDay() {
-     return $('#day >  option:nth-child(3)')
+     return $('#day')
+  }
+  get selectionDay(){
+    return $('#day option:nth-child(3)')
   }
   get selectHour() {
-     return $('#hour >  option:nth-child(3)')
+     return $('#hour');
   }
+  get selectionHour() {
+    return $('#hour option:nth-child(3)');
+ }
   get selectTrainer() {
-     return $('#trainer >  option:nth-child(3)')
+     return $('#trainer');
   }
+  get selectionTrainer() {
+    return $('#trainer option:nth-child(3)');
+ }
   get selectActivity() {
-     return $('#activity >  option:nth-child(3)')
+     return $('#activity');
   }
+  get selectionActivity() {
+    return $('#activity option:nth-child(3)');
+ }
   get selectSlots() {
-     return $('[data-testid="form-classes"] input')
+     return $('[data-testid="form-classes"] input');
   }
   get errorsMsg() {
-     return $('[data-testid="form-classes"] p')
+     return $('[data-testid="form-classes"] p');
   }
   get cancelButtonClass() {
-     return $('[data-testid="form-classes"] button')
+     return $('[data-testid="form-classes"] button');
   }
   get resetButtonClass() {
-     return $('[data-testid="form-classes"] button:nth-child(2)')
+     return $('[data-testid="form-classes"] button:nth-child(2)');
   }
   get saveButtonClass() {
-     return $('[data-test-id="classes-save-button"]')
+     return $('[data-test-id="classes-save-button"]');
   }
   get addButtonClass() {
-     return $('[data-testid="classes-add-button"]')
+     return $('[data-testid="classes-add-button"]');
   }
   get modalSuccess(){
-    return $('[data-testid="modal-success"]')
+    return $('[data-testid="modal-success"]');
    }
   get modalSuccessTitle(){
-    return $('[data-testid="modal-success"] > h3')
+    return $('[data-testid="modal-success"] h3');
   }
   get confirmButtonClass(){
-    return $('[data-testid="modal-success"] > button')
+    return $('[data-testid="modal-success"] button');
   }
   get modalConfirm(){
     return $('[data-testid="modal-confirm"]');
   }
   get acceptButtonClass(){
-    return $('[data-testid="modal-confirm"] > button')
+    return $('[data-testid="modal-confirm"] button');
   }
   get editButtonClass(){
   return $('[data-testid="container-table"] tbody tr:last-child button');
   }
   get deleteButtonClass(){
-  return $('[data-testid="container-table"] tbody tr:last-child button:nth-child(2)')
+  return $('[data-testid="container-table"] tbody tr:last-child button:nth-child(2)');
   }
   async acceptClassClick() {
   await this.acceptButtonClass.click();
@@ -89,23 +101,20 @@ class ClassesAdmin {
   async addClassClick() {
       await this.addButtonClass.click();
   }
-  async fillSelectSlots() {
-  await this.selectSlots.setValue()
-  }
   async selectionDayClass(){
-  await this.selectDay.click();
+  await this.selectionDay.click();
   }
   async selectionHourClass(){
-  await this.selectHour.click();
+  await this.selectionHour.click();
   }
   async selectionTrainerClass(){
-  await this.selectTrainer.click();
+  await this.selectionTrainer.click();
   }
   async selectionActivityClass(){
-  await this.selectActivity.click();
+  await this.selectionActivity.click();
   }
-  async fillSlotsClass() {
-  await this.selectSlots.setValue()
+  async fillSlotsClass(slot) {
+  await this.selectSlots.setValue(slot);
   }
 }
 
--- test/pageobjects/menuAdminPage.js
+++ test/pageobjects/menuAdminPage.js
@@ -1,18 +1,18 @@
 class NavBarAdmin {
   get activitiesButton() {
-      return $('[data-testid="container-aside-general"] > nav > ul > li:nth-child(1) > a');
+      return $('[data-testid="container-aside-general"] nav ul li:nth-child(1) a');
   }
   get classesButton() {
-      return $('[data-testid="container-aside-general"] > nav > ul > li:nth-child(2) > a');
+      return $('[data-testid="container-aside-general"] li:nth-child(2) a');
   }
   get membersButton() {
-      return $('[data-testid="container-aside-general"] > nav > ul > li:nth-child(3) > a');
+      return $('[data-testid="container-aside-general"] nav ul li:nth-child(3) a');
   }
   get subscriptionsButton() {
-      return $('[data-testid="container-aside-general"] > nav > ul > li:nth-child(4) > a');
+      return $('[data-testid="container-aside-general"] nav ul li:nth-child(4) a');
   }
   get trainersButton() {
-      return $('[data-testid="container-aside-general"] > nav > ul > li:nth-child(5) > a');
+      return $('[data-testid="container-aside-general"] nav ul li:nth-child(5) a');
   }
   async activityClick() {
       await this.activitiesButton.click();
