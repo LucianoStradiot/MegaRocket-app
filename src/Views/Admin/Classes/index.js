dce7440840c40a97d15af14a7d3c2020d6b13f34
Juan Cruz Tilatti <jctilatti@gmail.com>
2023-07-09 19:30:18 -0300

    MR-198: fixed a little bugs, created schedule in classes

--- src/Views/Admin/Classes/index.js
+++ src/Views/Admin/Classes/index.js
@@ -6,7 +6,7 @@ import Spinner from 'Components/Shared/Spinner';
 import { Link } from 'react-router-dom';
 import { deleteClass, getClasses } from 'Redux/Classes/thunks';
 import { useDispatch, useSelector } from 'react-redux';
-import Table from 'Components/Shared/Table';
+/* import Table from 'Components/Shared/Table'; */
 import Aside from 'Components/Shared/Aside';
 
 function Classes() {
@@ -14,7 +14,7 @@ function Classes() {
   const classes = useSelector((state) => state.classes.data);
   const loading = useSelector((state) => state.classes.isLoading);
 
-  const [idDelete, setIdDelete] = useState('');
+  const [idDelete /* , setIdDelete */] = useState('');
   const [isOpen, setIsOpen] = useState(false);
   const [responseModal, setResponseModal] = useState({
     title: '',
@@ -49,7 +49,7 @@ function Classes() {
     dispatch(getClasses());
   }, []);
 
-  const openModalConfirm = (id) => {
+  /* const openModalConfirm = (id) => {
     setIdDelete(id);
     setResponseModal({
       title: 'Confirm',
@@ -57,13 +57,30 @@ function Classes() {
       isConfirm: true
     });
     setIsOpen(true);
-  };
+  }; */
 
-  return classes.length > 0 ? (
+  const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const timeSlots = [
+    '09:00',
+    '10:00',
+    '11:00',
+    '12:00',
+    '13:00',
+    '14:00',
+    '15:00',
+    '16:00',
+    '17:00',
+    '18:00',
+    '19:00',
+    '20:00',
+    '21:00'
+  ];
+
+  return (
     <>
       <Aside page={'admins'} />
-      <div className={styles.container}>
-        <section className={styles.subContainer}>
+      <div className={styles.mainContainer}>
+        <section className={styles.container}>
           <Modal
             title={responseModal.title}
             desc={responseModal.description}
@@ -72,43 +89,81 @@ function Classes() {
             handleClose={() => setIsOpen(!isOpen)}
             deleteFunction={() => handleDeleteClass()}
           />
-          {loading && <Spinner />}
-          {!loading && (
-            <section>
-              <Link to="/admins/classes/form">
-                <Button text="Create" type="create" testId="create-button-classes" />
-              </Link>
-              <Table
+          <div className={styles.screenContainer}>
+            {loading && <Spinner />}
+            {!loading && (
+              <section>
+                {/* <Table
                 list={classes}
                 column={['Day', 'Hour', 'Trainer', 'Activity', 'Slots', '']}
                 fields={['day', 'hour', 'trainer.lastName', 'activity.name', 'slots']}
                 link={'/admins/classes/form/'}
                 action={openModalConfirm}
-              />
-            </section>
-          )}
-        </section>
-      </div>
-    </>
-  ) : (
-    <>
-      <Aside page={'admins'} />
-      <div className={styles.container}>
-        <section>
-          <Modal
-            title={responseModal.title}
-            desc={responseModal.description}
-            isOpen={isOpen}
-            confirmModal={responseModal.isConfirm}
-            handleClose={() => setIsOpen(!isOpen)}
-            deleteFunction={() => handleDeleteClass()}
-          />
-          <section>
-            <Link to="/admins/classes/form">
-              <Button text="Create" type="create" />
-            </Link>
-            <p className={styles.info}>There is no Class yet.</p>
-          </section>
+              /> */}
+                <table>
+                  <thead>
+                    <tr className={styles.tr}>
+                      <th className={styles.background}>
+                        <div>
+                          <Link to="/admins/classes/form">
+                            <Button text="Create" type="create" testId="create-button-classes" />
+                          </Link>
+                        </div>
+                      </th>
+                      {weekDays.map((day) => (
+                        <th key={day} className={styles.th}>
+                          {day}
+                        </th>
+                      ))}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {timeSlots.map((hour) => (
+                      <>
+                        <tr className={styles.tr}>
+                          <th className={styles.th}>{hour}</th>
+                          {weekDays.map((day, dayIndex) => (
+                            <td key={dayIndex}>
+                              {classes
+                                .filter(
+                                  (oneClass) => oneClass.day === day && oneClass.hour === hour
+                                )
+                                .map((oneClass, index) => {
+                                  return (
+                                    <div className={styles.card} key={index}>
+                                      <button className={styles.classCard}>
+                                        <p className={styles.inlineBlock}>
+                                          <div>{`Activity: ${
+                                            oneClass && oneClass.activity
+                                              ? oneClass.activity.name
+                                              : 'not available'
+                                          }`}</div>
+                                          <div>{`Trainer: ${
+                                            oneClass && oneClass.trainer
+                                              ? oneClass.trainer.firstName
+                                              : 'not available'
+                                          }`}</div>
+                                          <div>{`Slots: ${oneClass.slots}`}</div>
+                                        </p>
+                                      </button>
+                                    </div>
+                                  );
+                                })}
+                            </td>
+                          ))}
+                        </tr>
+                      </>
+                    ))}
+                    <tr className={styles.tr}>
+                      <td colSpan={weekDays.length + 1}>
+                        {classes.length === 0 && <p>There are no classes yet.</p>}
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </section>
+            )}
+          </div>
         </section>
       </div>
     </>
