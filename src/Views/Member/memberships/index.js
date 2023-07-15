312e92f5d6d5900ada4d2e3791b32f0004911ab8
LucianoStradiot <lstradiot15@gmail.com>
2023-07-14 21:50:51 -0300

    MR-221: Added validation for memberships when you are a member and when you are not

--- src/Views/Member/memberships/index.js
+++ src/Views/Member/memberships/index.js
@@ -1,11 +1,196 @@
 import Aside from 'Components/Shared/Aside';
 import styles from './memberships.module.css';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { useSelector } from 'react-redux';
+import Modal from 'Components/Shared/Modal';
+import { useDispatch } from 'react-redux';
+import { updateMember } from 'Redux/Members/thunks';
+import { getAuth } from 'Redux/Auth/thunks';
 
 const MembershipMember = () => {
+  const history = useHistory();
+  const data = useSelector((state) => state.user.user);
+  const [modal, setModal] = useState({
+    title: '',
+    desc: '',
+    isConfirm: false,
+    membership: ''
+  });
+  const [isOpen, setIsOpen] = useState(false);
+  const [refresh, setRefresh] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleCardOnly = () => {
+    if (data && sessionStorage.getItem('role') === 'MEMBER') {
+      if (data.membership !== 'Only Classes Membership') {
+        setModal({
+          title: 'Update',
+          desc: (
+            <div>
+              Are you sure you want to change your Membership to{' '}
+              <span className={styles.cont}>Only Classes Membership</span>?
+            </div>
+          ),
+          isConfirm: true,
+          membership: 'Only Classes Membership'
+        });
+      } else {
+        setModal({
+          title: 'Error',
+          desc: 'You already have this Membership',
+          isConfirm: false,
+          membership: ''
+        });
+        setRefresh(false);
+      }
+      setIsOpen(true);
+    } else if (data && sessionStorage.getItem('role') !== 'MEMBER') {
+      setModal({
+        title: 'Error',
+        desc: 'You need to be a member to have a Membership',
+        isConfirm: false,
+        membership: ''
+      });
+      setRefresh(false);
+      setIsOpen(true);
+    } else {
+      history.push('/signUp?membership=Only Classes Membership');
+    }
+  };
+
+  const handleCardClassic = () => {
+    if (data && sessionStorage.getItem('role') === 'MEMBER') {
+      if (data.membership !== 'Classic Membership') {
+        setModal({
+          title: 'Update',
+          desc: (
+            <div>
+              Are you sure you want to change your Membership to{' '}
+              <span className={styles.cont}>Classic Membership</span>?
+            </div>
+          ),
+          isConfirm: true,
+          membership: 'Classic Membership'
+        });
+      } else {
+        setModal({
+          title: 'Error',
+          desc: 'You already have this Membership',
+          isConfirm: false,
+          membership: ''
+        });
+        setRefresh(false);
+      }
+      setIsOpen(true);
+    } else if (data && sessionStorage.getItem('role') !== 'MEMBER') {
+      setModal({
+        title: 'Error',
+        desc: 'You need to be a member to have a Membership',
+        isConfirm: false,
+        membership: ''
+      });
+      setRefresh(false);
+      setIsOpen(true);
+    } else {
+      history.push('/signUp?membership=Classic Membership');
+    }
+  };
+
+  const handleCardBlack = () => {
+    if (data && sessionStorage.getItem('role') === 'MEMBER') {
+      if (data.membership !== 'Black Membership') {
+        setModal({
+          title: 'Update',
+          desc: (
+            <div>
+              Are you sure you want to change your Membership to{' '}
+              <span className={styles.cont}>Black Membership</span>?
+            </div>
+          ),
+          isConfirm: true,
+          membership: 'Black Membership'
+        });
+      } else {
+        setModal({
+          title: 'Error',
+          desc: 'You already have this Membership',
+          isConfirm: false,
+          membership: ''
+        });
+        setRefresh(false);
+      }
+      setIsOpen(true);
+    } else if (data && sessionStorage.getItem('role') !== 'MEMBER') {
+      setModal({
+        title: 'Error',
+        desc: 'You need to be a member to have a Membership',
+        isConfirm: false,
+        membership: ''
+      });
+      setRefresh(false);
+      setIsOpen(true);
+    } else {
+      history.push('/signUp?membership=Black Membership');
+    }
+  };
+
+  const changeMembership = async () => {
+    try {
+      const payload = {
+        id: data._id,
+        body: {
+          membership: modal.membership
+        }
+      };
+
+      const response = await dispatch(updateMember(payload));
+
+      if (response.error) {
+        throw new Error(response.message);
+      } else {
+        setModal({
+          title: 'Success!',
+          desc: 'Membership updated successfully',
+          isConfirm: false,
+          membership: ''
+        });
+        setRefresh(true);
+      }
+    } catch (error) {
+      setModal({
+        title: 'Error!',
+        desc: error.message,
+        isConfirm: false,
+        membership: ''
+      });
+    }
+    setIsOpen(true);
+  };
+
+  const closeF = () => {
+    if (refresh) {
+      window.location.reload();
+    } else {
+      setIsOpen(!isOpen);
+    }
+  };
+
+  useEffect(() => {
+    dispatch(getAuth());
+  }, []);
+
   return (
     <>
       <Aside page={'home'} />
+      <Modal
+        title={modal.title}
+        desc={modal.desc}
+        isOpen={isOpen}
+        confirmModal={modal.isConfirm}
+        handleClose={closeF}
+        deleteFunction={changeMembership}
+      />
       <div className={styles.mainContainer}>
         <div className={styles.container} data-testid="memberships-container-page">
           <div className={styles.header}>
@@ -17,47 +202,39 @@ const MembershipMember = () => {
             </p>
           </div>
           <section className={styles.cardsCont}>
-            <Link
+            <div
+              onClick={() => handleCardOnly()}
               className={`${styles.cardContainer} ${styles.only}`}
-              to={
-                sessionStorage.getItem('role') ? `/` : `/signUp?membership=Only Classes Membership`
-              }
             >
-              <div>
-                <h3>ONLY CLASSES</h3>
-                <p>$2500</p>
-                <hr />
-                <p>Subscribe to classes.</p>
-                <p>Access to the grid class.</p>
-              </div>
-            </Link>
-            <Link
-              to={sessionStorage.getItem('role') ? `/` : `/signUp?membership=Classic Membership`}
+              <h3>ONLY CLASSES</h3>
+              <p>$2500</p>
+              <hr />
+              <p>Subscribe to classes.</p>
+              <p>Access to the grid class.</p>
+            </div>
+            <div
+              onClick={() => handleCardClassic()}
               className={`${styles.cardContainer} ${styles.classic}`}
             >
-              <div>
-                <h3>CLASSIC</h3>
-                <p>$4000</p>
-                <hr />
-                <p>Free access to the weight room.</p>
-                <p>Personalized follow-up by a trainer.</p>
-                <p>Grid visualization.</p>
-              </div>
-            </Link>
-            <Link
-              to={sessionStorage.getItem('role') ? `/` : `/signUp?membership=Black Membership`}
+              <h3>CLASSIC</h3>
+              <p>$4000</p>
+              <hr />
+              <p>Free access to the weight room.</p>
+              <p>Personalized follow-up by a trainer.</p>
+              <p>Grid visualization.</p>
+            </div>
+            <div
+              onClick={() => handleCardBlack()}
               className={`${styles.cardContainer} ${styles.black}`}
             >
-              <div>
-                <h3>BLACK</h3>
-                <p>$6000</p>
-                <hr />
-                <p>Free access to the weight room.</p>
-                <p>Free access to classes with prior registration.</p>
-                <p>Personalized follow-up by a trainer.</p>
-                <p>Visualization of the grid.</p>
-              </div>
-            </Link>
+              <h3>BLACK</h3>
+              <p>$6000</p>
+              <hr />
+              <p>Free access to the weight room.</p>
+              <p>Free access to classes with prior registration.</p>
+              <p>Personalized follow-up by a trainer.</p>
+              <p>Visualization of the grid.</p>
+            </div>
           </section>
         </div>
       </div>
